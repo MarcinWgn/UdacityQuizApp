d2600b8816716b3986509b4b7f4ff82464dadf3f
Marcin <wireamg@gmail.com>
2017-05-15 21:00:57 +0200

    add answer to Toast

--- app/src/main/java/com/example/marcin/wegrzyn/quizapp/MainActivity.java
+++ app/src/main/java/com/example/marcin/wegrzyn/quizapp/MainActivity.java
@@ -77,7 +77,9 @@ public class MainActivity extends AppCompatActivity {
             else textToshow +=getString(R.string.answ_nr)+String.valueOf(i+1)+getString(R.string.not_correct)+nl;
         }
         textToshow+=nl+getString(R.string.result_is)+String.valueOf(score)+getString(R.string.for_for)+nl;
+
         submitTextView.setText(textToshow);
+        Toast.makeText(getBaseContext(),textToshow, Toast.LENGTH_LONG).show();
 
         if(score == answer.length)
             Toast.makeText(getBaseContext(), R.string.awesome,Toast.LENGTH_LONG).show();
